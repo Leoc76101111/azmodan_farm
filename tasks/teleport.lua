806b835da0cd3fc585dd6b7bbc79268983ece9e2
Kolden12 <143414254+Kolden12@users.noreply.github.com>
2025-12-27 00:02:05 -0600

    Update teleport.lua

--- tasks/teleport.lua
+++ tasks/teleport.lua
@@ -1,20 +1,23 @@
-local plugin_label = 'azmodan_farm' -- change to your plugin name
-
+local plugin_label = 'azmodan_farm'
 local utils = require "core.utils"
 
-local status_enum = {
-    IDLE = 'idle'
-}
 local task = {
-    name = 'teleport', -- change to your choice of task name
-    status = status_enum['IDLE']
+    name = 'teleport',
+    status = 'idle'
 }
+
 function task.shouldExecute()
-    return not utils.player_in_zone('Hawe_Zarbinzet') and
-        not utils.player_in_zone('Hawe_WorldBoss')
+    local fight_center = vec3:new(-217.6220, 616.8730, 22)
+    
+    -- Only teleport if we are NOT in town AND NOT in the boss zone
+    -- And we must be far enough (>150 units) that walking back isn't feasible
+    return not utils.player_in_zone('Hawe_WorldBoss') and 
+           not utils.player_in_zone('Hawe_Zarbinzet') and
+           utils.distance_to(fight_center) > 150
 end
 
 function task.Execute()
+    console.print("Player lost. Teleporting to Zarbinzet to restart path.")
     teleport_to_waypoint(0xA46E5)
 end
 
